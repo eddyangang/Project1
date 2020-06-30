0082f84f45dcf57df469ddc6a5ecbcf74c6e558e
eddyangang <eddyyang@berkeley.edu>
2020-06-30 03:34:07 -0700

    Made it easier to see response from API

--- script.js
+++ script.js
@@ -1,39 +1,70 @@
+/* Documentation for Google Civic API is here:
 
+https://developers.google.com/civic-information/docs/v2
+
+*/
 
 $(document).ready(function () {
-    var APIKey = "AIzaSyBXX_LFscJIXrN_xa7JvFqda1GJXYE8L0Y";
-    // var address = "1263 Pacific Ave. Kansas City KS";
-    var address = "94806"
-    var electionURL = `https://www.googleapis.com/civicinfo/v2/elections?key=${APIKey}`;
-    
 
-    var electionId = 2000;
+    var APIKey = "AIzaSyBXX_LFscJIXrN_xa7JvFqda1GJXYE8L0Y";
+
+    // Example Address
+    // var address = "1263 Pacific Ave. Kansas City KS";
+    var address = "120 sproul hall Berkeley CA 94704"
+
+    // Election ID can be obtained from eletion query response, and be fed into voter query
+    var electionId = 4973;
+    // Returns list of available elections.
+    var electionURL = `https://www.googleapis.com/civicinfo/v2/elections?key=${APIKey}`;
+
+    // Returns polling places (if included). contest and cadidate information, and election official information.
     var voterURL = `https://www.googleapis.com/civicinfo/v2/voterinfo?address=${address}&key=${APIKey}&officialOnly=true&returnAllAvailableData=true&electionId=${electionId}`;
 
-    var representativesURL = `https://www.googleapis.com/civicinfo/v2/representatives?key=${APIKey}&address=${address}`
-    // `https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=336%20Hawk%20Ridge%20Dr.&key=AIzaSyBXX_LFscJIXrN_xa7JvFqda1GJXYE8L0Y`
 
-    // var voterURL = `https://civicinfo.googleapis.com/civicinfo/v2/voterinfo?address=${address}&officialOnly=true&returnAllAvailableData=true&key=${APIKey}`
+    // Looks up political geography and representative information for a single address
+    var representativesInfoByAddressURL = `https://www.googleapis.com/civicinfo/v2/representatives?key=${APIKey}&address=${address}`
     
-    $.ajax({
-        url: voterURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
 
-
-
+    $('#voter').on("click", function () {
         $.ajax({
-            url: representativesURL,
+            url: voterURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            
+
         })
-        
-        
-        
     })
+
+    $('#ele').on("click", function () {
+        $.ajax({
+            url: electionURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+        })
+
+    })
+
+    $('#rep').on("click", function () {
+
+        $.ajax({
+            url: representativesInfoByAddressURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+        })
+
+    })
+
+
+
 });
 
+/* NOTES:
 
+528 does not have a public api. Instead their data is shared using excels sheets and can be downloaded. (it seems).
+
+
+proPublica imay be the next best API to use for real-time nomination data, bill data + other things.*/
