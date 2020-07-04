da3324f711c208f399efbc1b595d966fd42362d9
Preeti Gupta <preetigupta510@gmail.com>
2020-07-03 19:59:56 -0700

    adding function to get billing information from name

--- script.js
+++ script.js
@@ -26,7 +26,138 @@ var voterURL = `https://www.googleapis.com/civicinfo/v2/voterinfo?key=${civicKey
 
 var representativesInfoByAddressURL = `https://www.googleapis.com/civicinfo/v2/representatives?key=${civicKey}&address=${address}`
 $(document).ready(function () {
-   
+   /**
+    * 
+    * Work to get billing information when name is provided
+    */
+
+    // this method is used to handle ajax sucess when proPublicaMemberurl is called;
+    function getBillInformationByMemberIdSuccess(response) {
+        console.log(response);
+        var table = $('#billInformationTable');
+        table.append(`<tr>
+                    <th>
+                        Bill No
+                    </th>
+                    <th>
+                        Intorduced On
+                    </th>
+                <th>
+                    Title
+                </th>
+                <th>
+                    Description
+                </th>
+                <th>
+                    Commitee
+                </th>
+                <th>
+                    Sponsor
+                </th>
+                <th>
+                    Last Action
+                </th>
+                <th>
+                    Last Action Date
+                </th>
+                </tr>`);
+
+        for (let i = 0; i < 20; i++) {
+            table.append(`<tr>
+                    <td>
+                        <a href='${response.results[0].bills[i].congressdotgov_url}'>${response.results[0].bills[i].number}</a>
+                    </td>
+                    <td>
+                    ${response.results[0].bills[i].introduced_date}
+                    </td>
+                <td>
+                ${response.results[0].bills[i].short_title}
+                </td>
+                <td>
+                ${response.results[0].bills[i].title}
+                </td>
+                <td>
+                ${response.results[0].bills[i].committees}
+                </td>
+                <td>
+                ${response.results[0].bills[i].sponsor_name}
+                </td>
+                <td>
+                ${response.results[0].bills[i].number}
+                </td>
+                </tr>`);
+        }
+    };
+
+    // this method is used to handle ajax failure when proPublicaMemberurl is called;
+    function getBillInformationByMemberIdFailure(response) {
+        console.log(response);
+    };
+
+
+    function getBillInformationByMemberId(memberId) {
+        var proPublicaBillsByMembers = `https://api.propublica.org/congress/v1/members/${memberId}/bills/introduced.json`;
+        $.ajax({
+            url: proPublicaBillsByMembers,
+            method: "GET",
+            dataType: 'json',
+            headers: {
+                'X-API-Key': proPublicaKey
+            }
+        }).then(getBillInformationByMemberIdSuccess, getBillInformationByMemberIdFailure);
+
+    }
+
+    function getMemberIdByName(url,name,isSenate) {     
+        $.ajax({
+            url: url,
+            method: "GET",
+            dataType: 'json',
+            headers: {
+                'X-API-Key': proPublicaKey
+            }
+        }).then(function (response) {
+            console.log(response);
+            proPublicMembersUrlSuccess(response, name,isSenate);
+        }, function (response) {
+            proPublicMembersUrlFailue(response, name);
+        });
+    }
+
+    function proPublicMembersUrlSuccess(response, name,isSenate) {
+        var len = response.results[0].members.length;
+        var arr = response.results[0].members;
+        console.log(arr);
+        for(var i = 0; i < len; i++)
+        {
+            var memberName = arr[i].first_name;
+            if(arr[i].middle_name != null)
+            {
+                memberName = memberName + arr[i].middle_name;
+            }
+            memberName = memberName + arr[i].last_name;
+            memberName = memberName.split(' ').join('');
+            if(name === memberName){
+                getBillInformationByMemberId(arr[i].id);
+                return;
+            }
+        }
+        if(isSenate)
+            getMemberIdByName(proPublicaMembersUrlForHouse,name,false); 
+    };
+
+    function proPublicMembersUrlFailue(response, name) {
+        return '';
+    }
+
+    // Test Case when billing information is clicked
+    // $('#btn').on("click", function () {
+    //     var name = 'Jefferson Van Drew';
+    //     name = name.split(' ').join('');
+    //     console.log(name);
+    //     getMemberIdByName(proPublicaMembersUrlForSenate,name,true);        
+    // })
+
     function search(arr) {
         return arr.state === state;
     }
